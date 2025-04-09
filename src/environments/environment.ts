4f108f52351e591f29957a42522e7288b6396ef0
Jacob Shaeffer <shaeffer.jacob@gmail.com>
2025-04-09 14:39:46 -0700

    fixed metadata values listed in advanced search

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,8 @@
 
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost/backend/',
+  // apiUrl: 'http://localhost:8080/backend/',
+  apiUrl: 'http://www.solarcubed.org/east-africa/backend/',
   contentUrl: 'http://www.solarcubed.org/dev-content-ea/',
   moduleUrl: 'http://www.solarcubed.org/east-africa/',
 };
